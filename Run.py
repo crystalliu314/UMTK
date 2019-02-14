e8aa201b90668adcdb3184eba39362fc8f3f7fc9
Unknown <monburgers@gmail.com>
2019-02-14 15:24:47 -0500

    Python Live Graphing

--- Run.py
+++ Run.py
@@ -6,26 +6,59 @@ import datetime
 import time
 import sys
 import serial
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 
 #Setup for Serial Communication with Arduino (Using USB Cable)
 arduinoSerial = serial.Serial('/dev/cu.usbmodem14201',9600)
+#plt.ion()
+
+def animateGraph(file):
+    style.use('fivethirtyeight')
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+
+    def animate(i):
+        graph_data = open(file,'r').read()
+        lines = graph_data.split('\n')
+        lines.pop(0)
+        xs = []
+        ys = []
+        for line in lines:
+            if len(line) > 1:
+                x, y = line.split(',')
+                xs.append(float(x))
+                ys.append(float(y))
+        ax1.clear()
+        ax1.plot(xs, ys)
+
+    ani = animation.FuncAnimation(fig, animate, interval=500)
+    plt.show()
 
 def main():
     #---SETUP---
     record = False
     print "Press button to start recording data."
     #---SETUP---
-    
+
     #---LOOP---
     while True:
         #Waits for Arduino to send data through Serial
         if arduinoSerial.inWaiting():
             #Reads single line from serial
             currentSerialLine = arduinoSerial.readline()
-            
+
             if currentSerialLine == "BEGIN\n":
                 record = True
                 print "Starting Test"
+
+                #Initialize Graph
+                style.use('fivethirtyeight')
+                fig = plt.figure()
+                ax1 = fig.add_subplot(1,1,1)
+
                 #Initializing File Name to Current Date and File Path
                 currentDate = datetime.datetime.now()
                 fileName = str(currentDate) + '.csv'
@@ -35,7 +68,7 @@ def main():
                 print "Displacement (mm), Scale Reading (kg)"
                 header = "Displacement (mm), Scale Reading (kg)"
                 datasheetFile.write(header)
-            
+
             if record == True:
                 if currentSerialLine == "END\n":
                     print "Ending Test"
@@ -44,6 +77,27 @@ def main():
                 else:
                     print currentSerialLine
                     datasheetFile.write(currentSerialLine)
+                    datasheetFile.close()
+
+                    animateGraph(completeFileNameAndPath)
+
+                    graph_data = open(completeFileNameAndPath,'r').read()
+                    lines = graph_data.split('\n')
+                    lines.pop(0)
+                    xs = []
+                    ys = []
+                    for line in lines:
+                        if len(line) > 1:
+                            x, y = line.split(',')
+                            xs.append(float(x))
+                            ys.append(float(y))
+                    plt.grid(True)
+                    plt.ylabel("Scale Reading")
+                    plt.xlabel("Displacement")
+                    plt.plot(xs, ys)
+                    plt.pause(0.000001)
+
+                    datasheetFile = open(completeFileNameAndPath, "a")
     #---LOOP---
 
 if __name__ == '__main__':
